75a456e1fc5e129340c272c24a5ae16cc81a2362
Juan720v <Juan.Vela@mail.citytech.cuny.edu>
2022-03-14 16:42:57 -0400

    temperature both versions

---
+++ temperature.py
@@ -0,0 +1,5 @@
+tempincelsius = float(input("Please enter a temperature in Celsius: "))
+tempinfahrenheit = (tempincelsius * 1.8) + 32
+tempinfahrenheit = round(tempinfahrenheit)
+
+print("The temperature is " + str(tempinfahrenheit) + "F")
